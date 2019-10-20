66b5f6de6a7b56d14dd3e16ba91599fbb36b086c
andy.boot <bootandy@gmail.com>
2019-10-20 10:33:07 +0100

    Fix Camera initialization

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use self::data::old_vec3::Point;
 use self::data::ray::Ray;
 use self::data::sphere::HittableObjects;
 use data::world::build_world;
-use std::f64::consts::PI;
+use std::f32::consts::PI;
 
 pub mod data;
 
@@ -52,53 +52,60 @@ const BLACK: Color = Color {
 };
 const APERTURE: f32 = 0.3;
 
-fn rnd_in_disk() -> Point {
-    let mut p = Point {
-        x: 1.0,
-        y: 1.0,
-        z: 1.0,
-    };
-    while p.squared_length() >= 1.0 {
-        p = Point {
+fn random_in_unit_disk() -> Point {
+    loop {
+        let p = Point {
             x: random::<f32>() * 2.0 - 1.0,
             y: random::<f32>() * 2.0 - 1.0,
             z: 0.0,
         };
+        if p.dot(&p) < 1.0 {
+            return p;
+        }
     }
-    p
 }
 
 struct Camera {
-    low_left: Point,
-    horiz: Point,
-    vert: Point,
     origin: Point,
+    lower_left: Point,
+    horizontal: Point,
+    vertical: Point,
+    u: Point,
+    v: Point,
     lens_radius: f32,
 }
 
 impl Camera {
-    fn new(look_from: Point, look_at: Point, vfov: f32, aspect: f32, aperture: f32) -> Camera {
-        let dist_focus = (look_from - look_at).len();
-
-        let theta = vfov * PI as f32 / 180.0;
-        let half_height = (theta/2.0).tan();
-        let half_width = aspect * half_height;
-        let origin = look_from;
-        let w = (look_from - look_at).unit_vector();
-        let u = UP.cross(&w).unit_vector();
-        let v = w.cross(&u);
-        let low_left = ORIGIN - u*half_width*dist_focus - v*half_height *dist_focus- w*dist_focus;
-        let horiz = u * 2.0 * half_width*dist_focus;
-        let vert = v * 2.0 * half_height*dist_focus;
-        Camera{low_left, horiz, vert, origin, lens_radius: aperture/2.0}
+    fn get_ray(&self, x: f32, y: f32) -> Ray {
+        let rd = random_in_unit_disk() * self.lens_radius;
+        let offset = self.u * rd.x + self.v * rd.y;
+        Ray {
+            origin: offset + self.origin,
+            direction: self.lower_left + self.horizontal * x + self.vertical * y - self.origin - offset,
+        }
     }
 
-    fn get_ray(&self, x: f32, y: f32) -> Ray {
-        let rd = rnd_in_disk() * self.lens_radius;
-        let offset = x*rd.x + y * rd.y;
-        Ray {
-            origin: Point{x:offset, y:offset, z:offset} + self.origin,
-            direction: self.low_left + self.horiz * x + self.vert * y - self.origin - Point{x:offset, y:offset, z:offset},
+    fn new(look_from: Point, look_at: Point, vfov: f32, aspect: f32, aperture: f32) -> Camera {
+        let focus_dist = (look_from - look_at).len();
+        let lens_radius = aperture / 2.0;
+        let theta = vfov * PI / 180.0;
+        let half_height = f32::tan(theta / 2.0);
+        let half_width = aspect * half_height;
+        let w = (look_from - look_at).unit_vector();
+        let u = (UP.cross(&w)).unit_vector();
+        let v = w.cross(&u);
+
+        let lower_left = look_from - (u * half_width * focus_dist) - (v * half_height * focus_dist) - w * focus_dist;
+        let horizontal = u * (2.0 * focus_dist * half_width);
+        let vertical = v * (2.0 * focus_dist * half_height);
+        Camera {
+            lower_left,
+            horizontal,
+            vertical,
+            origin: look_from,
+            lens_radius,
+            u,
+            v,
         }
     }
 }
@@ -148,9 +155,13 @@ fn main() -> std::io::Result<()> {
     let mut buffer = File::create("out.ppm")?;
     buffer.write_all(format!("P3\n{} {}\n255\n", NX, NY).as_bytes())?;
     let world = build_world();
-    let look_from = Point{x:3.0, y:3.0, z:2.0};
-    let look_at = Point{x:0.0, y:0.0, z:-1.0};
-    let cam = Camera::new(look_from, look_at, 20.0, NX as f32/NY as f32, APERTURE);
+    let cam = Camera::new(
+        Point{x:0.0, y: 0.0, z: 1.0},
+        Point{x:0.0, y: 0.0, z: -1.0},
+        90.0,
+        NX as f32 / NY as f32,
+        APERTURE,
+    );
 
     for y in (0..NY).rev() {
         for x in 0..NX {
